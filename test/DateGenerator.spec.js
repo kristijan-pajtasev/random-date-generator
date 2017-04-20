2e9563fbc4e9b2b0997b004603ba8f0dba72ac71
Kristijan Pajtasev <Kristijan Pajtasev>
2017-04-20 03:05:11 +0100

    tests

---
+++ test/DateGenerator.spec.js
@@ -0,0 +1,20 @@
+let DateGenerator = require('../src/DateGenerator');
+let assert = require('assert');
+
+describe('DateGenerator', function() {
+
+    it('getRandomDate returns date', function() {
+        let date = DateGenerator.getRandomDate();
+        assert.equal(true, date instanceof Date && !isNaN(date.valueOf()));
+    });
+
+    it('getRandomDate returns between given dates', function() {
+        let startDate = new Date(2017, 2, 12);
+        let endDate = new Date(2017, 3, 22);
+        for(let i = 0; i < 100; i++) {
+            let date = DateGenerator.getRandomDateInRange(startDate, endDate);
+            assert.equal(true, date <= endDate && date >= startDate);
+        }
+    });
+
+});
--- test/test.js
+++
@@ -1,6 +0,0 @@
-var assert = require('assert');
-describe('Test', function() {
-    it('true should be true', function() {
-        assert.equal(true, true);
-    });
-});
