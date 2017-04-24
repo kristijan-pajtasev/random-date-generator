19aef1796581c40e259450ba16c2082bc1bce09e
Kristijan Pajtasev <Kristijan Pajtasev>
2017-04-24 02:17:35 +0100

    bug fix

--- src/DateGenerator.js
+++ src/DateGenerator.js
@@ -3,8 +3,8 @@ module.exports = {
         return new Date(Math.random() * new Date().getTime())
     },
     getRandomDateInRange(startDate, endDate) {
-        if(!!startDate) startDate = new Date();
-        if(!!endDate) endDate = new Date();
+        if(!startDate) startDate = new Date();
+        if(!endDate) endDate = new Date();
 
         let diff = endDate.getTime() - startDate.getTime();
         return new Date(Math.random() * diff + startDate.getTime());
