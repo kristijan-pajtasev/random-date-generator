9d3eaa266f87f38ce91960bdb4e1d05ea94aef40
Kristijan Pajtasev <Kristijan Pajtasev>
2017-04-24 02:14:08 +0100

    default start and end date

--- src/DateGenerator.js
+++ src/DateGenerator.js
@@ -3,6 +3,9 @@ module.exports = {
         return new Date(Math.random() * new Date().getTime())
     },
     getRandomDateInRange(startDate, endDate) {
+        if(!!startDate) startDate = new Date();
+        if(!!endDate) endDate = new Date();
+
         let diff = endDate.getTime() - startDate.getTime();
         return new Date(Math.random() * diff + startDate.getTime());
     }
